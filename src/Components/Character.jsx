66d2268139c4b67203c2480e7cae8dc9aa48fd74
zhaqyy <zaqrashyy7@gmail.com>
2024-05-02 13:18:19 +0100

    text scroll area completed

---
+++ src/Components/Character.jsx
@@ -0,0 +1,57 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { useScroll, useTransform, motion } from 'framer-motion';
+import React, { useRef } from 'react';
+import "../Style/Component/Component.css";
+
+
+export default function Paragraph({paragraph}) {
+
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start 0.65", "start 0.25"]
+  })
+
+  const words = paragraph.split(" ")
+  return (
+    <h2 
+      ref={container}         
+      className='paragraph'
+    >
+    {
+      words.map( (word, i) => {
+        const start = i / words.length
+        const end = start + (1 / words.length)
+        return <Word key={i} progress={scrollYProgress} range={[start, end]}>{word}</Word>
+      })
+    }
+    </h2>
+  )
+}
+
+const Word = ({children, progress, range}) => {
+  const amount = range[1] - range[0]
+  const step = amount / children.length
+  return (
+    <span className='word'>
+      {
+        children.split("").map((char, i) => {
+          const start = range[0] + (i * step);
+          const end = range[0] + ((i + 1) * step)
+          return <Char key={`c_${i}`} progress={progress} range={[start, end]}>{char}</Char>
+        })
+      }
+    </span>
+  )
+}
+
+const Char = ({children, progress, range}) => {
+  const opacity = useTransform(progress, range, [0,1])
+  return (
+    <span>
+      <span className='shadow'>{children}</span>
+      <motion.span style={{opacity: opacity}}>{children}</motion.span>
+    </span>
+  )
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,26 +6,60 @@ import "../Style/Home/Home.css";
 import logo from "/logodark.png";
 import { useRef } from "react";
 import useScrollSnap from "react-use-scroll-snap";
+import Paragraph from "../Components/Character";
+import "../Style/Component/Component.css";
+
+
+const paragraph = `Fournir 
+les 
+solutions 
+d’emballage 
+les 
+plus 
+efficaces`
 
 export default function Home() {
-    const scrollRef = useRef(null);
-    // useScrollSnap({ ref: scrollRef, duration: 10, delay: 10 });
-  
+  const scrollRef = useRef(null);
+  // useScrollSnap({ ref: scrollRef, duration: 10, delay: 10 });
+
   const TargetRef = useRef();
   const { scrollYProgress } = useScroll({
     target: TargetRef,
     offset: ["end end", "end start"],
   });
   const MoveY = useTransform(scrollYProgress, [0, 0.1], ["0vh", "-100vh"]);
-  const MoveVidY = useTransform(scrollYProgress, [0.1, 0.2,0.99,1], ["10%", "0%", "0%", "5%"]);
-  const wide = useTransform(scrollYProgress, [0.1, 0.2,0.6, 1], ["15%", "100%","100%","40%"]);
-  const x = useTransform(scrollYProgress, [ 0.1, 0.2,0.6,1], ["50%","50%","50%","25%"]);
-  const tall = useTransform(scrollYProgress, [0.1, 0.2, 0.59, 0.6], ["300px", "500px", "500px", "700px"]);
+  const MoveVidY = useTransform(
+    scrollYProgress,
+    [0.1, 0.2, 0.99, 1],
+    ["10%", "0%", "0%", "5%"]
+  );
+  const position = useTransform(scrollYProgress, (pos) => {
+    return pos === 1 ? "relative" : "fixed";
+  });
+  const wide = useTransform(
+    scrollYProgress,
+    [0.1, 0.2, 0.6, 1],
+    ["15%", "100%", "100%", "40%"]
+  );
+  const x = useTransform(
+    scrollYProgress,
+    [0.1, 0.2, 0.6, 1],
+    ["50%", "50%", "50%", "25%"]
+  );
+  const tall = useTransform(
+    scrollYProgress,
+    [0.1, 0.2, 0.59, 0.6],
+    ["300px", "500px", "500px", "700px"]
+  );
   const bright = useTransform(scrollYProgress, [0.1, 0.2], ["0.25", "0.75"]);
-// const fil = filter=`brightness(${bright})`;
+  // const fil = filter=`brightness(${bright})`;
   return (
     <>
-      <motion.section className="wrapper"  ref={scrollRef} style={{padding:'0'}}>
+      <motion.section
+        className="wrapper"
+        ref={scrollRef}
+        style={{ padding: "0" }}
+      >
         <motion.section className="hero" ref={TargetRef}>
           <motion.div className="hero-head" style={{ y: MoveY }}>
             <h1 className="hollow">
@@ -36,7 +70,20 @@ export default function Home() {
               TO
             </h1>
           </motion.div>
-          <motion.video autoPlay loop muted playsInline className="hero-vid" style={{top:MoveVidY, width: wide, height:tall,left:x}}>
+          <motion.video
+            autoPlay
+            loop
+            muted
+            playsInline
+            className="hero-vid"
+            style={{
+              top: MoveVidY,
+              width: wide,
+              height: tall,
+              left: x,
+            //   position,
+            }}
+          >
             <source src="hero.webm" type="video/webm" />
             <source src="hero.mp4" type="video/mp4" />
           </motion.video>
@@ -52,23 +99,22 @@ export default function Home() {
           </motion.div>
           <motion.img src={logo} style={{ y: MoveY }} />
         </motion.section>
-      <section id="canvasWrapper">
-        <Scene />
-      </section>
-      <motion.section className="vid">
-
-      </motion.section>
-      <section className="textline">
-        <h2>
-        Fournir <br />
-les <br />
-solutions <br />
-d’emballage <br />
-les <br />
-plus <br />
-efficaces
-        </h2>
-      </section>
+        <section id="canvasWrapper">
+          <Scene />
+        </section>
+        <motion.section className="vid"></motion.section>
+        <section className="textline">
+        <Paragraph paragraph={paragraph} />
+          {/* <h2>
+            Fournir <br />
+            les <br />
+            solutions <br />
+            d’emballage <br />
+            les <br />
+            plus <br />
+            efficaces
+          </h2> */}
+        </section>
       </motion.section>
     </>
   );
